934c8f251f721530568509357648ef1573a36f06
suvidha <suvidhadhakane@gmail.com>
2026-07-12 00:14:45 +0530

    serialization and deserialization

--- util/Main.java
+++ util/Main.java
@@ -1,20 +1,36 @@
 
 import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+
 public class Main {
     public static void main(String[] args) {
-        String inputImagePath = "spice_factory.jpg";       // make sure this file exists
-        String outputImagePath = "compress.png";     // will be overwritten
+        String inputImagePath = "spice_factory.jpg"; // make sure this file exists
+        String outputImagePath = "compress.png"; // will be overwritten
         float threshold = 1.0f;
 
         try {
             CompressImage compressor = new CompressImage(inputImagePath, threshold);
             compressor.build();
             compressor.saveCompressed(outputImagePath);
-            compressor.saveEdge("edges_only.png", 2);
+            // compressor.saveEdge("edges_only.png", 2);
             compressor.saveBinary("compressed.qtc");
+
+            CompressImage loaded = CompressImage.loadBinary("compressed.qtc", "png");
+            BufferedImage reconstructed = loaded.img;
+            Graphics2D g = reconstructed.createGraphics();
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, reconstructed.getWidth(), reconstructed.getHeight());
+            g.dispose();
+            loaded.qt.render(reconstructed);
+            ImageIO.write(reconstructed, "png", new File("test_reconstructed_from_binary.png"));
+
             System.out.println("Binary file saved to: " + new File("compressed.qtc").getAbsolutePath());
             System.out.println("Compression complete. Output saved to " + outputImagePath);
         } catch (IOException e) {
