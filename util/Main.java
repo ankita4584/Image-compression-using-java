dd6cbe6975c20b181acaba616cef751f45cb6d6d
suvidha <suvidhadhakane@gmail.com>
2026-07-11 21:22:05 +0530

    serializefile

--- util/Main.java
+++ util/Main.java
@@ -1,6 +1,8 @@
 
 import java.util.*;
+import java.io.File;
 import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         String inputImagePath = "spice_factory.jpg";       // make sure this file exists
@@ -12,7 +14,8 @@ public class Main {
             compressor.build();
             compressor.saveCompressed(outputImagePath);
             compressor.saveEdge("edges_only.png", 2);
-
+            compressor.saveBinary("compressed.qtc");
+            System.out.println("Binary file saved to: " + new File("compressed.qtc").getAbsolutePath());
             System.out.println("Compression complete. Output saved to " + outputImagePath);
         } catch (IOException e) {
             System.err.println("Error during compression: " + e.getMessage());
